b959ff8c2d436ea79de0e0daf9ad8756ba15a826
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-03-28 09:10:58 +0700

    Add Tauri

--- docs/_next/static/6gpyv0X-owAUGXpqR9oZ6/_buildManifest.js
+++ docs/_next/static/6gpyv0X-owAUGXpqR9oZ6/_buildManifest.js
@@ -12,7 +12,7 @@ self.__BUILD_MANIFEST = {
     "static/chunks/0mzxztp_6cv00.js"
   ],
   "/app": [
-    "static/chunks/04-fys27-_dyp.js"
+    "static/chunks/0g5x_bzvv2p-~.js"
   ],
   "/version": [
     "static/chunks/03huro4.g.bk4.js"
--- docs/_next/static/chunks/0g5x_bzvv2p-~.js
+++ docs/_next/static/chunks/0g5x_bzvv2p-~.js
@@ -1,6 +1,6 @@
 __turbopack_load_page_chunks__("/app", [
-  "static/chunks/0ewfu9h94s0db.js",
+  "static/chunks/0bdf~r7yq_k_v.js",
   "static/chunks/11iu7265eje54.js",
   "static/chunks/17~~v-lk950fs.js",
-  "static/chunks/turbopack-0.c_2n6nrh13k.js"
+  "static/chunks/turbopack-0a3v1umg3i3v1.js"
 ])
