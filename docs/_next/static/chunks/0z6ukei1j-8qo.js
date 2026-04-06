1c226874896caf32c18e5cc4c3b559aae696945e
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2026-04-06 16:25:15 +0700

    Update CI

--- docs/_next/static/chunks/0z6ukei1j-8qo.js
+++ docs/_next/static/chunks/0z6ukei1j-8qo.js
@@ -1,7 +1,8 @@
 __turbopack_load_page_chunks__("/_app", [
-  "static/chunks/0w29hcm78~kbn.js",
+  "static/chunks/0hk3ozrg7vr1d.js",
   "static/chunks/07eau680k.01x.js",
   "static/chunks/0p9zjw.lu99so.js",
   "static/chunks/0fjftkl_.rftl.css",
-  "static/chunks/turbopack-0wjricn3.mqe4.js"
+  "static/chunks/0eki28umy5xci.css",
+  "static/chunks/turbopack-0ua9ohv1tbi6x.js"
 ])
--- packages/app/src/pages/_app.tsx
+++ packages/app/src/pages/_app.tsx
@@ -1,13 +1,26 @@
 import '@frankfurter/styles/globals.css';
-import { HeadTemplate } from '../templates/HeadTemplate';
 import type { AppProps } from 'next/app';
+import { Geist, Geist_Mono } from 'next/font/google';
 import { FC } from 'react';
+import { HeadTemplate } from '../templates/HeadTemplate';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
 
 const App: FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <>
       <HeadTemplate basic={{ title: 'Frankfurter' }} />
-      <Component {...pageProps} />
+      <div className={`${geistSans.className} ${geistMono.className}`}>
+        <Component {...pageProps} />
+      </div>
     </>
   );
 };
